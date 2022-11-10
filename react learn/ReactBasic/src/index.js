d8c740a1460b8a98ec5e2bf5bd701cc6a4c2e1c5
Sam-Sisodia <sajal89304@gmail.com>
2022-11-10 11:27:54 +0530

    basic_react

---
+++ react learn/ReactBasic/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import ReactDOM from "react-dom";
+import App from "./App";
+
+
+// let el = <h1>Hello Sajal You are become Full stack Devloper </h1>
+
+
+
+ReactDOM.render(<App />,document.getElementById("root"));
+
