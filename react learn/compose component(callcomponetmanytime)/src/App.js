6548efb4366bbe9be091a65618e03759c19e1d93
Sam-Sisodia <sajal89304@gmail.com>
2022-11-10 12:48:53 +0530

    call_componets

---
+++ react learn/compose component(callcomponetmanytime)/src/App.js
@@ -0,0 +1,19 @@
+import Student from "./student";
+
+
+
+const App = ()=>
+{
+    return (
+        <div>
+            <Student />
+            <Student />
+
+            <Student name = "por1"/>
+
+            <Student name= "pro2" />
+        </div>
+    )
+}
+
+export default App
---
+++ react learn/compose component(callcomponetmanytime)/src/student.js
@@ -0,0 +1,10 @@
+//Composing componet 
+
+import React from "react"
+
+const Student = props =>
+{
+    return <h1>Sajal  Sisodia {props.name}is here </h1>
+}
+
+export default Student
